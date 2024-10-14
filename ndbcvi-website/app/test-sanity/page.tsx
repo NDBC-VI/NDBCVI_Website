795bd594a1fd4308ea4ccc6b1f0408fe6836dbbd
PamiFalade <jesupamilerin.falade@ucalgary.ca>
2024-10-14 23:29:18 +0100

    Creating modals for every infoPopup, and fixed modal behaviour

--- ndbcvi-website/app/test-sanity/page.tsx
+++ ndbcvi-website/app/test-sanity/page.tsx
@@ -4,30 +4,38 @@ import { allInfoPopupsQuery } from '@/sanity/lib/queries';
 import { InfoModal } from "../views/InfoModal";
 import { InfoPopupProps } from '../types';
 import { SanityDocument } from 'next-sanity';
+import infoPopup from '@/sanity/schemaTypes/infoPopup';
 
 export const revalidate = 60;
 
 export default async function Home() {
   
-  const allInfoPopups = await sanityFetch<SanityDocument[]>({ query: allInfoPopupsQuery });
+  const allInfoPopups: SanityDocument[] = await sanityFetch<SanityDocument[]>({ query: allInfoPopupsQuery });
 
   const activePopup = allInfoPopups.find(popup => popup.slug.current === "membership-popup");
-
-  const infoModalProps: InfoPopupProps = {
-    introduction: activePopup?.introduction,
-    title: activePopup?.title,
-    slug: { 
-        current: activePopup?.slug.current,
-    },
-    headerImages: activePopup?.headerImages,
-    infoSections: [...activePopup?.infoSections]
-  }
-
   
   return (
-    <div className="w-full pt-10 pb-20 flex flex-col items-center bg-black">
-
-      <InfoModal {...infoModalProps} />
+    <div className="w-full h-[300vh] pt-10 pb-20 flex flex-col items-center bg-green-200">
+      <div className='w-full absolute top-[90%] flex flex-row items-center bg-white'>
+        {
+          allInfoPopups.map((infoPopup: SanityDocument) => {
+            const infoModalProps: InfoPopupProps = {
+              introduction: infoPopup?.introduction,
+              title: infoPopup?.title,
+              slug: { 
+                  current: infoPopup?.slug.current,
+              },
+              headerImages: infoPopup?.headerImages,
+              infoSections: [...infoPopup?.infoSections]
+            }
+            return (
+              <div key={infoPopup.slug.current} className='relative h-screen grow border-2'>
+                <InfoModal {...infoModalProps} />
+              </div>
+            );
+          })
+        }
+      </div>
       {/* <iframe src="https://calendar.google.com/calendar/embed?src=ndbcvidev%40gmail.com&ctz=Africa%2FLagos" style={{border:"0", width:"80vw", height:"60vh"}}></iframe> */}
     
     </div>
--- ndbcvi-website/app/views/InfoModal.tsx
+++ ndbcvi-website/app/views/InfoModal.tsx
@@ -1,16 +1,28 @@
 'use client';
 
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { createPortal } from 'react-dom';
 import Link from 'next/link';
 import Image from 'next/image'; 
 import { InfoSection } from '../components/InfoSection';
 import { urlFor } from '@/sanity/lib/image';
 import { InfoSectionType, InfoPopupProps } from '../types';
+import { create } from 'domain';
 
 
 
 
-export const InfoModal: React.FC<InfoPopupProps> = (content: InfoPopupProps) => {
+export const InfoModal = (props: InfoPopupProps) => {
+
+    const {introduction, title, slug, headerImages, infoSections } = props;
+
+    const [open, setOpen] = useState(false);
+
+    // Wait for the DOM to be ready before teleporting modal to the top of the DOM
+    const [domReady, setDomReady] = useState(false)
+    useEffect(() => {
+        setDomReady(true)
+    }, [])
 
 
     const scrollIntoTheView = (e: React.MouseEvent, id: string) => {
@@ -27,60 +39,75 @@ export const InfoModal: React.FC<InfoPopupProps> = (content: InfoPopupProps) => 
     };
 
     return (
-        <div id="infoPopup" className="w-11/12 pb-2 rounded-3xl bg-white">
-            <div className="h-[60vh] px-9 py-10 rounded-t-3xl">
-                <div className="h-[15%] w-full flex flex-row justify-between items-center">
-                    <h1 className="text-4xl font-bold">{content.title}</h1>
-                    <button className='w-8 h-8 rounded-full bg-slate-100'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="grey" className="size-6 mx-auto">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                <div className="h-[80%] my-5 w-full flex flex-row justify-around items-end">
-                    <Image 
-                        src={urlFor(content.headerImages[0].asset._ref).url()}
-                        alt="placeholder image"
-                        width={450}
-                        height={200}
-                    />
 
-                    <Image 
-                        src={urlFor(content.headerImages[1].asset._ref).url()}
-                        alt="placeholder image"
-                        width={350}
-                        height={500}
-                    />
-                </div>
-            </div>
-            <div className="relative grid grid-cols-3 grid-rows-1">
-                <div className="sticky top-0 h-96 p-4 grid">
-                    <h3 className="font-medium text-lg">On this page</h3>
-                    {
-                        content.introduction?.slug?.current !== undefined &&
-                        <Link key={0} className="text-thin text-sm" onClick={(e) => scrollIntoTheView(e, content.introduction.slug.current)} href={""}>{content.introduction.title}</Link>
-                    }
-                    {
-                        content.infoSections?.map((section: InfoSectionType, index: number) => (
-                            <Link key={index+1} className="text-thin text-sm" onClick={(e) => scrollIntoTheView(e, section.slug.current)} href={""}>{section.title}</Link>
-                        ))
-                    }
-                </div>
-                <div className="col-span-2 h-auto p-6 relative self-end">
-                    {
-                        content.introduction?.slug?.current !== undefined &&
-                        <div key={content.introduction.slug.current} >
-                            <InfoSection infoSection={content.introduction} />
-                        </div>
-                    }
-                    { content.infoSections?.map(section => (
-                                <div key={section.slug.current} >
-                                    <InfoSection infoSection={section} />
+        <>
+            <button type='button' className='h-10 px-4 font-medium text-sm rounded-md text-white bg-gray-900' onClick={() => setOpen(true)}>
+                Open Modal
+            </button>
+
+            {
+                domReady &&
+                createPortal(
+                    <div id='backdrop' onClick={() => setOpen(false)} className={` flex flex-col items-center overflow-scroll transition-colors overflow-scroll ${open ? "fixed inset-0 bg-black/50" : ""}`}>
+                        <div id="infoPopup" onClick={(e) => e.stopPropagation()} className={`absolute top-0 left-1/12 w-11/12 pb-2 rounded-3xl bg-white ${open ? "" : 'hidden'}`}>
+                            <div className="h-[60vh] px-9 py-10 rounded-t-3xl">
+                                <div className="h-[15%] w-full flex flex-row justify-between items-center">
+                                    <h1 className="text-4xl font-bold">{title}</h1>
+                                    <button onClick={() => setOpen(false)} className='w-8 h-8 rounded-full bg-slate-100'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="grey" className="size-6 mx-auto">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
                                 </div>
-                            ))
-                    }
-                </div> 
-            </div> 
-        </div>
+                                <div className="h-[80%] my-5 w-full flex flex-row justify-around items-end">
+                                    <Image 
+                                        src={urlFor(headerImages[0].asset._ref).url()}
+                                        alt="placeholder image"
+                                        width={450}
+                                        height={200}
+                                    />
+
+                                    <Image 
+                                        src={urlFor(headerImages[1].asset._ref).url()}
+                                        alt="placeholder image"
+                                        width={350}
+                                        height={500}
+                                    />
+                                </div>
+                            </div>
+                            <div className="relative grid grid-cols-3 grid-rows-1">
+                                <div className="sticky top-0 h-96 p-4 grid">
+                                    <h3 className="font-medium text-lg">On this page</h3>
+                                    {
+                                        introduction?.slug?.current !== undefined &&
+                                        <Link key={0} className="text-thin text-sm" onClick={(e) => scrollIntoTheView(e, introduction.slug.current)} href={""}>{introduction.title}</Link>
+                                    }
+                                    {
+                                        infoSections?.map((section: InfoSectionType, index: number) => (
+                                            <Link key={index+1} className="text-thin text-sm" onClick={(e) => scrollIntoTheView(e, section.slug.current)} href={""}>{section.title}</Link>
+                                        ))
+                                    }
+                                </div>
+                                <div className="col-span-2 h-auto p-6 relative self-end">
+                                    {
+                                        introduction?.slug?.current !== undefined &&
+                                        <div key={introduction.slug.current} >
+                                            <InfoSection infoSection={introduction} />
+                                        </div>
+                                    }
+                                    { infoSections?.map(section => (
+                                                <div key={section.slug.current} >
+                                                    <InfoSection infoSection={section} />
+                                                </div>
+                                            ))
+                                    }
+                                </div> 
+                            </div> 
+                        </div>
+                    </div>,
+                    document.getElementById('portal')
+                )
+            }
+        </>
     )
 }
