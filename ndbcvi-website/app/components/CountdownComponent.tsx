12e0e0031b3d4558017400782e18b6cf1a90a31f
PamiFalade <jesupamilerin.falade@ucalgary.ca>
2024-10-24 16:25:11 +0100

    Padded numbers so that they are always two digits

--- ndbcvi-website/app/components/CountdownComponent.tsx
+++ ndbcvi-website/app/components/CountdownComponent.tsx
@@ -3,14 +3,23 @@ import Countdown from 'react-countdown'
 
 
 export const CountdownComponent = ({dateTime}: {dateTime: Date}) => {
-    const startDate = new Date(dateTime);
+
+    const padToTwoDigits = (number: number) => {
+        if(number < 10) {
+            return `0${number}`;
+        }
+        else {
+            return number;
+        }
+    }
 
     const renderer = ({ days, hours, minutes, seconds, completed }: { days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
         return (
             <div className='w-full p-2'>
-                <h3 className='text-4xl font-semibold text-slate-300 my-2'><span className='text-black'>{days}</span> days</h3>
-                <h3 className='text-4xl font-semibold text-slate-300 my-2'><span className='text-black'>{hours}</span> hours</h3>
-                <h3 className='text-4xl font-semibold text-slate-300 my-2'><span className='text-black'>{minutes}</span> minutes</h3>
+                <h3 className='text-4xl font-semibold text-slate-300 my-2'><span className='text-black'>{padToTwoDigits(days)}</span> days</h3>
+                <h3 className='text-4xl font-semibold text-slate-300 my-2'><span className='text-black'>{padToTwoDigits(hours)}</span> hours</h3>
+                <h3 className='text-4xl font-semibold text-slate-300 my-2'><span className='text-black'>{padToTwoDigits(minutes)}</span> minutes</h3>
+                <h3 className='text-4xl font-semibold text-slate-300 my-2'><span className='text-black'>{padToTwoDigits(seconds)}</span> seconds</h3>
             </div>
         )
     }
