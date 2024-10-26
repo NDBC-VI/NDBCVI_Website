271fd65b50d95b5db95ecf2baf404bd8d030704f
PamiFalade <jesupamilerin.falade@ucalgary.ca>
2024-10-26 15:24:44 +0100

    Renamed CountdownComponent in EventsModal

--- ndbcvi-website/app/shared/modals/EventsModal/components/EventsModalCountdownComponent.tsx
+++ ndbcvi-website/app/shared/modals/EventsModal/components/EventsModalCountdownComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Countdown, { zeroPad } from 'react-countdown'
 
 
-export const CountdownComponent = ({startDate}: {startDate: Date}) => {
+export const EventsModalCountdownComponent = ({startDate}: {startDate: Date}) => {
 
 
     const renderer = ({ days, hours, minutes, seconds, completed }: { days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
