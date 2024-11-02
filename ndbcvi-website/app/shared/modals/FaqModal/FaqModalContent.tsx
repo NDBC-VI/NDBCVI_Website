3cd2b096234ebe67f4a2964a6434c3d97eff308c
NDBC-VI <ndbcvidev@gmail.com>
2024-11-02 08:25:00 +0100

    Redeploy Vercel

--- ndbcvi-website/app/shared/modals/FaqModal/FaqModalContent.tsx
+++ ndbcvi-website/app/shared/modals/FaqModal/FaqModalContent.tsx
@@ -12,7 +12,7 @@ export const FaqModalContent = ({faqs}: {faqs: SanityDocument}) => {
 
   return (
       <>
-          <div className="relative grid grid-cols-3 grid-rows-1 px-9 py-10">
+          <div className='relative grid grid-cols-3 grid-rows-1 px-9 py-10'>
             <ScrollLinks sections={[{title: "FAQs", slug: "faqs"}, ...faqInfoSections.map((section: InfoSectionType) => ({title: section.title, slug: section.slug.current}))]}/>
             <div id="faqs" className="col-span-2 h-auto p-6 relative self-end">
               {/* FAQ Tabbed Section Component */}
