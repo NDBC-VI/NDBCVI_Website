ccfd22630bc36d42857862f4a81994455fb6d82a
NDBC-VI <ndbcvidev@gmail.com>
2024-11-02 10:39:45 +0100

    Redeploy Vercel

--- ndbcvi-website/sanity/lib/fetch.ts
+++ ndbcvi-website/sanity/lib/fetch.ts
@@ -28,7 +28,6 @@ export async function sanityFetch<QueryResponse>({
   return client
     .withConfig({ useCdn: true })
     .fetch<QueryResponse>(query, params, {
-      cache: isDevelopment || isDraftMode ? undefined : "force-cache",
       ...(isDraftMode && {
         token: token,
         perspective: "previewDrafts",
