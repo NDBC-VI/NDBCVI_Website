d0fa44cd21de55e534135398a2dc84dae1386c58
PamiFalade <jesupamilerin.falade@ucalgary.ca>
2024-11-15 16:07:13 +0100

    Contact page mobile

--- ndbcvi-website/app/pages/information_center/sections/Maps.tsx
+++ ndbcvi-website/app/pages/information_center/sections/Maps.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Maps = () => {
   return (
-    <div className="max-w-[1512px] mx-auto">
+    // <div className="max-w-[1512px] mx-auto">
+    <div className="w-full mx-auto">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31717.877728656473!2d3.4498356!3d6.4281179!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf513ff9bd641%3A0x596799a9108ac311!2sNew%20Dawn%20Baptist%20Church!5e0!3m2!1sen!2sng!4v1730126104922!5m2!1sen!2sng"
         style={{ border: 0 }}
